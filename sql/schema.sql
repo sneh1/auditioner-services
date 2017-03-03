5a3bb6be8a1c78fc04733452bb464f8206315722
Tony Brobston <tonybrobston@sourceallies.com>
2017-03-03 14:03:39 -0600

    Added Production table

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,3 +9,11 @@ CREATE TABLE `Family` (
   `name` varchar(48) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Production` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(48) NOT NULL,
+  `auditionDate` date NOT NULL,
+  `season` varchar(48) NOT NULL
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
