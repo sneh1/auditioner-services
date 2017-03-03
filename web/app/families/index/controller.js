671ba65afca0edecf953921b04b4e4849692ea24
Steven Nelson <snelson@iowaballetacademy.com>
2017-03-03 12:50:27 -0600

    Adding family layout

--- web/app/families/index/controller.js
+++ web/app/families/index/controller.js
@@ -5,9 +5,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   familyService: Ember.inject.service('family-service'),
   actions:{
-    editFamily:function(family) {
-
-    },
     deleteFamily:function(family){
       const familyService = this.get('familyService');
       const model = this.get('model');
