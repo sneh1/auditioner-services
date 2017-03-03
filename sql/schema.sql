cfa9d097e3af95c987922eabeddb18d6b74f4478
Steven Nelson <snelson@iowaballetacademy.com>
2017-03-03 14:16:55 -0600

    Add grant

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,6 +4,8 @@ USER auditioner;
 
 CREATE USER 'auditioner'@'localhost' IDENTIFIED BY 'Password1!';
 
+GRANT ALL PRIVILEGES ON *.* TO 'auditioner'@'localhost'
+
 CREATE TABLE `Family` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(48) NOT NULL,
